c6e37108fa3f40fbadb3d88001608b0b62bed1d9
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-03-06 15:48:21 +0530

    Create displaytuple.py

---
+++ pratice/displaytuple.py
@@ -0,0 +1,7 @@
+# user to input the tuple elements separated by spaces
+tuple_data = tuple(input("Enter the tuple elements separated by spaces: ").split())
+
+# Print all the items in the tuple
+print("Tuple items:")
+for item in tuple_data:
+    print(item)
