b761943f3cd912c99855a51fe55d1d7e85915d74
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-03-09 12:26:12 +0530

    Delete pratice/listtostring.py

--- pratice/listtostring.py
+++
@@ -1,7 +0,0 @@
-# input the list elements separated by spaces
-word_list = input("Enter the words separated by spaces: ").split()
-
-# Convert list to string
-result_string = ' '.join(word_list)
-
-print("String:", result_string)
